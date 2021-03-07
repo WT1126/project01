88ddb7f1cd369f36cf58bc5dbf891e420e7c0508
sht1126 <2463495149@qq.com>
2021-03-07 12:09:38 +0800

    提交index类

--- src/demo/index.java
+++ src/demo/index.java
@@ -4,10 +4,11 @@ package demo;
 public class index {
     String  cat;
     String dog;
-
+     String pig;
     public static void main(String[] args) {
 index demo=new index();
 demo.cat="猫";
 demo.dog="狗";
+demo.pig="猪";
     }
 }
