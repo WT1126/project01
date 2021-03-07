d3c9b787d683e531cf0597de095a4ad1e1bda920
sht1126 <2463495149@qq.com>
2021-03-07 12:07:37 +0800

    提交head类

---
+++ src/demo/index.java
@@ -0,0 +1,13 @@
+
+package demo;
+
+public class index {
+    String  cat;
+    String dog;
+
+    public static void main(String[] args) {
+index demo=new index();
+demo.cat="猫";
+demo.dog="狗";
+    }
+}
