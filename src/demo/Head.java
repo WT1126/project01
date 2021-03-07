bf4e711d7d0096a9a931e536666cff0411f144ab
sht1126 <2463495149@qq.com>
2021-03-07 11:38:45 +0800

    修改head类

---
+++ src/demo/Head.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Head {
+      String name;
+      String sex;
+      int  age;
+ public static void main(String[] args) {
+     Head head= new Head();
+   head.name="汪涛";
+   head.sex="男";
+   head.age=25;
+        System.out.println(head.name+"::"+head.sex+":"+head.age);
+    }
+
+
+}
