778d682a3a1c07effb09bf39d8adf4cc34566c2a
WT1126 <1757249294@qq.com>
2021-03-08 21:36:43 +0800

    修改two类

--- src/demo/two.java
+++ src/demo/two.java
@@ -12,15 +12,20 @@ package demo;
 import java.util.Scanner;
 
 public class two {
-  String  sex;
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-             two p=new two();
-             System.out.println(p.sex="男");
-            String s=sc.nextLine();
-int a=0,b=0,c=0,k=0;
-            for (int i = 0; i <s.length() ; i++) {
-                if((s.charAt(i)<=90&s.charAt(i)>=65)|(s.charAt(i)<=122&s.charAt(i)>=97)|(s.charAt(i)<=57&s.charAt(i)>=48)|s.charAt(i)==32){
+
+    String name;
+    int age;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s=sc.nextLine();
+        two p=new two();
+        p.name="汪涛";
+        p.age=18;
+        System.out.println(p.name);
+        int a=0,b=0,c=0,k=0;
+        for (int i = 0; i <s.length() ; i++) {
+            if((s.charAt(i)<=90&s.charAt(i)>=65)|(s.charAt(i)<=122&s.charAt(i)>=97)|(s.charAt(i)<=57&s.charAt(i)>=48)|s.charAt(i)==32){
                 if(s.charAt(i)<=90&s.charAt(i)>=65){
                     a++;
                 }
@@ -33,19 +38,19 @@ int a=0,b=0,c=0,k=0;
                 if(s.charAt(i)==32){
                     c++;
                 }
-                }else{
+            }else{
 
-                        k++;
+                k++;
 
-                }
+            }
+
+
+        }
+        System.out.println("字母个数"+a+"\n"+"数字个数"+b+"\n"+"空格个数"+c+"\n"+"其他字符个数"+k);
+    }
 
 
 }
-            System.out.println("字母个数"+a+"\n"+"数字个数"+b+"\n"+"空格个数"+c+"\n"+"其他字符个数"+k);
-}
-
-
-           }
 
 
 
