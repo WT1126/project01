7ece36b8e1abd5cfbfbbd91295bffd6d31d59b85
WT1126 <1757249294@qq.com>
2021-03-10 19:18:59 +0800

    创建four类

---
+++ src/demo/four.java
@@ -0,0 +1,18 @@
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: WANGTAO
+ * \* Date: 2021/3/10
+ * \* Time: 19:12
+ * \* To change this template use File
+ * \*Settings | File Templates.
+ * \* Description:
+ **/
+package demo;
+
+public class four {
+    public static void main(String[] args) {
+        String name;
+        name="name";
+        System.out.println(name);
+    }
+}
